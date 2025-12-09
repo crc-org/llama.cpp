676605c0e1c77d42bbbbc03b2e5b49a8634c630a
Kevin Pouget <kpouget@redhat.com>
2025-12-09 14:46:52 +0100

    ggml: force disable vulkan loader when compiling the ggml-remotingbackend

--- ggml/src/ggml-backend-reg.cpp
+++ ggml/src/ggml-backend-reg.cpp
@@ -200,7 +200,13 @@ struct ggml_backend_registry {
         register_backend(ggml_backend_sycl_reg());
 #endif
 #ifdef GGML_USE_VULKAN
-        register_backend(ggml_backend_vk_reg());
+	/* HACK: when compiling the APIR frontend/backend
+	 * simultaneously, with vulkan, vulkan gets loaded in the
+	 * frontend side and the ggml-remotingfrontend receives
+	 * unknown VK buffers ... */
+#ifndef GGML_USE_REMOTINGBACKEND
+        //register_backend(ggml_backend_vk_reg());
+#endif
 #endif
 #ifdef GGML_USE_WEBGPU
         register_backend(ggml_backend_webgpu_reg());
