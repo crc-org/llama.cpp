3e7cc699cc5eca8276d553dad02a45f7afc9d3a3
Kevin Pouget <kpouget@redhat.com>
2026-02-25 20:29:05 +0100

    [ggml-virtgpu-backend] Add 'backend' object management

--- ggml/src/ggml-virtgpu/backend/backend-dispatched.gen.h
+++ ggml/src/ggml-virtgpu/backend/backend-dispatched.gen.h
@@ -30,7 +30,9 @@ uint32_t backend_buffer_clear(apir_encoder * enc, apir_decoder * dec, virgl_apir
 uint32_t backend_buffer_free_buffer(apir_encoder * enc, apir_decoder * dec, virgl_apir_context * ctx);
 
 /* backend */
+uint32_t backend_backend_initialize(apir_encoder * enc, apir_decoder * dec, virgl_apir_context * ctx);
 uint32_t backend_backend_graph_compute(apir_encoder * enc, apir_decoder * dec, virgl_apir_context * ctx);
+uint32_t backend_backend_cleanup(apir_encoder * enc, apir_decoder * dec, virgl_apir_context * ctx);
 
 extern "C" {
 static const backend_dispatch_t apir_backend_dispatch_table[APIR_BACKEND_DISPATCH_TABLE_COUNT] = {
@@ -68,6 +70,8 @@ static const backend_dispatch_t apir_backend_dispatch_table[APIR_BACKEND_DISPATC
 
     /* backend */
 
+    /* APIR_COMMAND_TYPE_BACKEND_INITIALIZE  = */ backend_backend_initialize,
     /* APIR_COMMAND_TYPE_BACKEND_GRAPH_COMPUTE  = */ backend_backend_graph_compute,
+    /* APIR_COMMAND_TYPE_BACKEND_CLEANUP  = */ backend_backend_cleanup,
 };
 }
